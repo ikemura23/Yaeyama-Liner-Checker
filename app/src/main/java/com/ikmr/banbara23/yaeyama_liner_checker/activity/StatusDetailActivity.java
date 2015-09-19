6155a4c2ab0b949185539f27c4dd20fcbd4c5349
banbara23 <banbara.work@gmail.com>
2015-09-19 23:57:05 +0900

    StatusListActivityとDetailActivityを追加

---
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/activity/StatusDetailActivity.java
@@ -0,0 +1,8 @@
+
+package com.ikmr.banbara23.yaeyama_liner_checker.activity;
+
+/**
+ * ステータス詳細のActivity
+ */
+public class StatusDetailActivity extends BaseActivity {
+}
---
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/activity/StatusListActivity.java
@@ -0,0 +1,9 @@
+
+package com.ikmr.banbara23.yaeyama_liner_checker.activity;
+
+/**
+ * ステータス一覧Activity
+ */
+public class StatusListActivity extends BaseActivity {
+
+}
