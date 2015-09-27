0d68dfc2d3c531bf72495fa1321ca8df7752cfda
banbara23 <banbara.work@gmail.com>
2015-09-27 15:49:48 +0900

    運航一覧viewの表示をある程度整えた

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/view/StatusListView.java
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/view/StatusListView.java
@@ -6,15 +6,19 @@ import android.util.AttributeSet;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.ikmr.banbara23.yaeyama_liner_checker.entity.Liner;
 import com.ikmr.banbara23.yaeyama_liner_checker.R;
+import com.ikmr.banbara23.yaeyama_liner_checker.entity.Liner;
+import com.ikmr.banbara23.yaeyama_liner_checker.entity.Port;
+import com.ikmr.banbara23.yaeyama_liner_checker.entity.Status;
 
 /**
  * ステータスリストのカスタムビュー
  */
 public class StatusListView extends LinearLayout {
     TextView mPortText;
-    TextView mStatusText;
+    TextView mStatus_normal;
+    TextView mStatus_cancel;
+    TextView mStatus_cation;
     TextView mCommentText;
 
     public StatusListView(Context context) {
@@ -29,13 +33,71 @@ public class StatusListView extends LinearLayout {
     protected void onFinishInflate() {
         super.onFinishInflate();
         mPortText = (TextView) findViewById(R.id.view_status_list_port_text);
-        mStatusText = (TextView) findViewById(R.id.view_status_list_status_text);
+        mStatus_normal = (TextView) findViewById(R.id.view_status_list_status_normal_text);
+        mStatus_cancel = (TextView) findViewById(R.id.view_status_list_status_cancel_text);
+        mStatus_cation = (TextView) findViewById(R.id.view_status_list_status_cation_text);
         mCommentText = (TextView) findViewById(R.id.view_status_list_comment_text);
     }
 
+    /**
+     * ビューに値を表示
+     * 
+     * @param liner
+     */
     public void bind(Liner liner) {
-        mPortText.setText(liner.getPort().getValue());
-        mStatusText.setText(liner.getStatus().getStatus());
-        mCommentText.setText(liner.getText());
+        setPort(liner.getPort());
+        setStatus(liner.getStatus());
+        setComment(liner.getText());
+    }
+
+    /**
+     * 港
+     * 
+     * @param port
+     */
+    private void setPort(Port port) {
+        if (port == null) {
+            return;
+        }
+        mPortText.setText(port.getValue());
+    }
+
+    /**
+     * 運航状況
+     *
+     * @param status
+     */
+    private void setStatus(Status status) {
+        if (status == null) {
+            return;
+        }
+        mStatus_normal.setVisibility(GONE);
+        mStatus_cancel.setVisibility(GONE);
+        mStatus_cation.setVisibility(GONE);
+
+        switch (status) {
+            case NORMAL:
+                mStatus_normal.setVisibility(VISIBLE);
+                break;
+            case CANCEL:
+                mStatus_cancel.setVisibility(VISIBLE);
+                break;
+            case CAUTION:
+                mStatus_cation.setVisibility(VISIBLE);
+                break;
+        }
+    }
+
+    /**
+     * コメント
+     *
+     * @param text
+     */
+    private void setComment(String text) {
+        if (text == null) {
+            mCommentText.setText("エラー");
+            return;
+        }
+        mCommentText.setText(text);
     }
 }
