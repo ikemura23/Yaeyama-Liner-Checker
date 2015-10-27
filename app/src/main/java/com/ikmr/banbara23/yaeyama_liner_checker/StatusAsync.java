1d367b0eedccf00bf5de4946dec1f9676ed4cb11
banbara23 <banbara.work@gmail.com>
2015-10-28 00:17:58 +0900

    Asyncクラス作成

---
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/StatusAsync.java
@@ -0,0 +1,66 @@
+
+package com.ikmr.banbara23.yaeyama_liner_checker;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
+public class StatusAsync extends AsyncTask<String, Integer, Document> {
+    // Activiyへのコールバック用interface
+    public interface AsyncTaskCallback {
+        void preExecute();
+
+        void postExecute(Document result);
+
+        // void progressUpdate(int progress);
+
+        // void cancel();
+    }
+
+    private AsyncTaskCallback callback = null;;
+
+    public StatusAsync(AsyncTaskCallback _callback) {
+        this.callback = _callback;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        callback.preExecute();
+    }
+
+    // @Override
+    // protected void onCancelled() {
+    // super.onCancelled();
+    // callback.cancel();
+    // }
+
+    @Override
+    protected void onPostExecute(Document result) {
+        super.onPostExecute(result);
+        callback.postExecute(result);
+    }
+
+    // @Override
+    // protected void onProgressUpdate(Integer... values) {
+    // super.onProgressUpdate(values);
+    // callback.progressUpdate(values[0]);
+    // }
+
+    @Override
+    protected Document doInBackground(String... params) {
+        Document doc = null;
+        Log.d("StatusAsyncTaskLoader", "loadInBackground");
+        try {
+            // HTML取得 タイムアウトは10秒
+            doc = Jsoup.connect(params[0]).timeout(10000).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return doc;
+    }
+}
