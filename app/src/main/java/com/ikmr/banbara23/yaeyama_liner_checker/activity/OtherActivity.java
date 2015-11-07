e5f5510c7b6f11bd232d19fca17e754a3e2797b1
banbara23 <banbara.work@gmail.com>
2015-11-07 10:27:50 +0900

    メール起動を実装

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/activity/OtherActivity.java
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/activity/OtherActivity.java
@@ -2,11 +2,12 @@
 package com.ikmr.banbara23.yaeyama_liner_checker.activity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.ikmr.banbara23.yaeyama_liner_checker.R;
-import com.ikmr.banbara23.yaeyama_liner_checker.entity.Company;
 
 /**
  * Created by banbara23 on 15/11/03.
@@ -26,8 +27,20 @@ public class OtherActivity extends BaseActivity {
      * @param view
      */
     public void mailClick(View view) {
-        Intent intent = new Intent(this, StatusListActivity.class);
-        intent.putExtra(StatusListActivity.PARAM_COMPANY, Company.YKF);
-        startActivity(intent);
+
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SENDTO);
+        String mailAddress = getString(R.string.mail_address);
+        intent.setData(Uri.parse("mailto:" + mailAddress));
+
+        String subject = getString(R.string.title_activity_top) + getString(R.string.EXTRA_SUBJECT);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+
+        try {
+            startActivity(intent);
+        } catch (Exception e) {
+            Toast.makeText(this, "メール起動でエラーが発生しました", Toast.LENGTH_SHORT).show();
+        }
     }
+
 }
