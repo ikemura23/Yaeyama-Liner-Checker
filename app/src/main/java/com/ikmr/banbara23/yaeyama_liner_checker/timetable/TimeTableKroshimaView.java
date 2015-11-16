5d01c2e011990d93554cb2466f3fb9ab44e829e6
banbara23 <banbara.work@gmail.com>
2015-11-16 14:45:36 +0900

    時刻表 竹富が完成

---
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/timetable/TimeTableKroshimaView.java
@@ -0,0 +1,20 @@
+
+package com.ikmr.banbara23.yaeyama_liner_checker.timetable;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * 時刻表 竹富
+ */
+public class TimeTableKroshimaView extends LinearLayout {
+
+    public TimeTableKroshimaView(Context context) {
+        super(context);
+    }
+
+    public TimeTableKroshimaView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
---
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/timetable/TimeTableTaketomiView.java
@@ -0,0 +1,20 @@
+
+package com.ikmr.banbara23.yaeyama_liner_checker.timetable;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * 時刻表 竹富
+ */
+public class TimeTableTaketomiView extends LinearLayout {
+
+    public TimeTableTaketomiView(Context context) {
+        super(context);
+    }
+
+    public TimeTableTaketomiView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
