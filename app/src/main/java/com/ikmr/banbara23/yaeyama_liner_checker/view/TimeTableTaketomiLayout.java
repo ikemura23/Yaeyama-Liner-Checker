b822a786ef2796fe0c7e27d1c1a446f0b42637ed
banbara23 <banbara.work@gmail.com>
2015-11-16 13:51:05 +0900

    竹富の時刻表作成中

---
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/view/TimeTableTaketomiLayout.java
@@ -0,0 +1,20 @@
+
+package com.ikmr.banbara23.yaeyama_liner_checker.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * 時刻表 竹富
+ */
+public class TimeTableTaketomiLayout extends LinearLayout {
+
+    public TimeTableTaketomiLayout(Context context) {
+        super(context);
+    }
+
+    public TimeTableTaketomiLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
