f20f92f05175ea3a58dcef66c58a9cc45c19302d
banbara23 <banbara.work@gmail.com>
2015-12-14 13:28:43 +0900

    #12 波照間時刻表をbutterknife化

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/timetable/TimeTableHaterumaView.java
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/timetable/TimeTableHaterumaView.java
@@ -5,14 +5,26 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 
 import com.ikmr.banbara23.yaeyama_liner_checker.R;
+
+import butterknife.Bind;
 
 /**
  * 時刻表 竹富
  */
 public class TimeTableHaterumaView extends LinearLayout {
+
+    @Bind(R.id.hateruma_row1)
+    FrameLayout mRow1;
+
+    @Bind(R.id.hateruma_row2)
+    FrameLayout mRow2;
+
+    @Bind(R.id.hateruma_row3)
+    FrameLayout mRow3;
 
     public TimeTableHaterumaView(Context context) {
         super(context);
