98e2240c3a46327538855a6a9126112883454298
banbara23 <banbara.work@gmail.com>
2015-12-15 12:00:19 +0900

    #12 竹富時刻表に一斉表示切り替え実装

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/timetable/TimeTableTaketomiView.java
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/timetable/TimeTableTaketomiView.java
@@ -9,10 +9,43 @@ import android.view.View;
 import com.ikmr.banbara23.yaeyama_liner_checker.R;
 import com.ikmr.banbara23.yaeyama_liner_checker.entity.Company;
 
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 /**
  * 時刻表 竹富
  */
 public class TimeTableTaketomiView extends TimeTableBaseView {
+
+    // 安栄
+    @Bind({
+            R.id.taketomi_anei_row1,
+            R.id.taketomi_anei_row3,
+            R.id.taketomi_anei_row5,
+            R.id.taketomi_anei_row7,
+            R.id.taketomi_anei_row9,
+            R.id.taketomi_anei_row11,
+            R.id.taketomi_anei_row12,
+            R.id.taketomi_anei_row15,
+            R.id.taketomi_anei_row17
+    })
+    List<View> aneiViews;
+
+    // 八重山観光フェリー
+    @Bind({
+            R.id.taketomi_ykf_row2,
+            R.id.taketomi_ykf_row4,
+            R.id.taketomi_ykf_row6,
+            R.id.taketomi_ykf_row8,
+            R.id.taketomi_ykf_row10,
+            R.id.taketomi_ykf_row13,
+            R.id.taketomi_ykf_row14,
+            R.id.taketomi_ykf_row16,
+            R.id.taketomi_ykf_row18
+    })
+    List<View> ykfViews;
 
     public TimeTableTaketomiView(Context context) {
         super(context);
@@ -21,10 +54,11 @@ public class TimeTableTaketomiView extends TimeTableBaseView {
     public TimeTableTaketomiView(Context context, AttributeSet attrs) {
         super(context, attrs);
         View layout = LayoutInflater.from(context).inflate(R.layout.view_time_table_taketomi, this);
+        ButterKnife.bind(this, layout);
     }
 
     @Override
     public void switchViews(Company company) {
-
+        switcCompany(company, aneiViews, ykfViews);
     }
 }
