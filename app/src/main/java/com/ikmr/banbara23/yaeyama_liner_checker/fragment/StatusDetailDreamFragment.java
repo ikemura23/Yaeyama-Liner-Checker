62969e531a7dc2c8602dec2bcb6989f7229929c1
池村 和剛 <kazutakai@tt-com.net>
2015-12-18 13:36:35 +0900

    #41 プレミアムドリームとスーパードリームの詳細に空のCardViewが見えていた対応

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/fragment/StatusDetailDreamFragment.java
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/fragment/StatusDetailDreamFragment.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +12,7 @@ import android.widget.LinearLayout;
 
 import com.ikmr.banbara23.yaeyama_liner_checker.R;
 import com.ikmr.banbara23.yaeyama_liner_checker.StringUtils;
+import com.ikmr.banbara23.yaeyama_liner_checker.entity.Port;
 import com.ikmr.banbara23.yaeyama_liner_checker.entity.YkfLinerDetail;
 import com.ikmr.banbara23.yaeyama_liner_checker.timetable.dream.DreamTimeTableView;
 import com.ikmr.banbara23.yaeyama_liner_checker.view.StatusDetailView;
@@ -79,7 +79,18 @@ public class StatusDetailDreamFragment extends BaseFragment {
         super.onResume();
         mFragmentDreamStatusDetailContentLayout.setVisibility(View.VISIBLE);
         mStatusDetailView.bind(getParam().getLiner(), createValueText());
-        mDreamTimeTableView.switchPortView(getParam().getPort());
+        if (isTimeTableShow()) {
+            mDreamTimeTableView.switchPortView(getParam().getPort());
+        }
+    }
+
+    /**
+     * 時刻表を表示する港か？ （プレミアムドリームとスーパードリームはじ時刻表を持っていないので表示しない）
+     * 
+     * @return true:表示 false:非表示
+     */
+    private boolean isTimeTableShow() {
+        return getParam().getPort() != Port.PREMIUM_DREAM && getParam().getPort() != Port.SUPER_DREAM;
     }
 
     private String createValueText() {
@@ -101,6 +112,9 @@ public class StatusDetailDreamFragment extends BaseFragment {
         return sb.toString();
     }
 
+    /**
+     * 外部電話アプリ起動
+     */
     private void startTell() {
         String tell;
         switch (getParam().getLiner().getCompany()) {
@@ -124,11 +138,13 @@ public class StatusDetailDreamFragment extends BaseFragment {
 
             startActivity(intent);
         } catch (Exception e) {
-            Log.d("StatusDetailYkfFragment", "e:" + e.getMessage());
             // 何もしない
         }
     }
 
+    /**
+     * 外部ブラウザアプリ起動
+     */
     private void startWeb() {
         String hpUrl;
         switch (getParam().getLiner().getCompany()) {
