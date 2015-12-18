ca7dae3a5420cb51349f868072dd902d80cc76ad
池村 和剛 <kazutakai@tt-com.net>
2015-12-18 11:59:11 +0900

    #41 ドリーム観光の時刻表のヘッダーstyleを名前変更

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/timetable/dream/DreamTimeTableUeharaView.java
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/timetable/dream/DreamTimeTableUeharaView.java
@@ -7,35 +7,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 
 import com.ikmr.banbara23.yaeyama_liner_checker.R;
-import com.ikmr.banbara23.yaeyama_liner_checker.entity.Company;
-import com.ikmr.banbara23.yaeyama_liner_checker.timetable.TimeTableBaseView;
 
-import java.util.List;
-
-import butterknife.Bind;
 import butterknife.ButterKnife;
 
 /**
- * 時刻表 竹富
+ * ドリーム観光 時刻表 上原・鳩間
  */
-public class DreamTimeTableUeharaView extends TimeTableBaseView {
-
-    // 安栄
-    @Bind({
-            R.id.uehara_anei_row1,
-            R.id.uehara_anei_row2,
-            R.id.uehara_anei_row4,
-            R.id.uehara_anei_row5,
-            R.id.uehara_anei_row7
-    })
-    List<View> aneiViews;
-
-    // 八重山観光フェリー
-    @Bind({
-            R.id.uehara_ykf_row3,
-            R.id.uehara_ykf_row6
-    })
-    List<View> ykfViews;
+public class DreamTimeTableUeharaView extends DreamTimeTableBaseView {
 
     public DreamTimeTableUeharaView(Context context) {
         super(context);
@@ -43,12 +21,7 @@ public class DreamTimeTableUeharaView extends TimeTableBaseView {
 
     public DreamTimeTableUeharaView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        View layout = LayoutInflater.from(context).inflate(R.layout.view_time_table_uehara, this);
+        View layout = LayoutInflater.from(context).inflate(R.layout.view_time_table_dream_uehara, this);
         ButterKnife.bind(this, layout);
-    }
-
-    @Override
-    public void switchViews(Company company) {
-        switcCompany(company, aneiViews, ykfViews);
     }
 }
