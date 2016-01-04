ed106f0b84abc70a982e3490a0abdc74ab654144
池村 和剛 <kazutakai@tt-com.net>
2016-01-04 10:17:50 +0900

    #45 値段クラスを作成

---
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/entity/FerryPrice.java
@@ -0,0 +1,26 @@
+
+package com.ikmr.banbara23.yaeyama_liner_checker.entity;
+
+/**
+ * 船の料金
+ */
+public class FerryPrice {
+    private String adult;
+    private String child;
+
+    public String getAdult() {
+        return adult;
+    }
+
+    public void setAdult(String adult) {
+        this.adult = adult;
+    }
+
+    public String getChild() {
+        return child;
+    }
+
+    public void setChild(String child) {
+        this.child = child;
+    }
+}
