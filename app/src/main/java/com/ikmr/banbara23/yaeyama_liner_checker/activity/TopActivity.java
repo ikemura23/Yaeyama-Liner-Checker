5270e808dc8124685b5fc64310926e13d94a8fac
池村 和剛 <kazutakai@tt-com.net>
2016-01-16 01:03:33 +0900

    #44 アプリトップから広告を削除

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/activity/TopActivity.java
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/activity/TopActivity.java
@@ -7,8 +7,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 import com.ikmr.banbara23.yaeyama_liner_checker.R;
 import com.ikmr.banbara23.yaeyama_liner_checker.entity.Company;
 import com.ikmr.banbara23.yaeyama_liner_checker.timetable.TimeTableActivity;
@@ -19,41 +17,23 @@ public class TopActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top);
-        loadAd();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_top, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        // noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * 広告読み込み
-     */
-    protected void loadAd() {
-        AdView mAdView = (AdView) findViewById(R.id.adView);
-        if (mAdView == null) {
-            return;
-        }
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mAdView.loadAd(adRequest);
     }
 
     /**
@@ -64,11 +44,6 @@ public class TopActivity extends BaseActivity {
     public void anneiClick(View view) {
         Intent intent = new Intent(this, StatusListActivity.class);
         intent.putExtra(StatusListActivity.PARAM_COMPANY, Company.ANNEI);
-        // todo:shared elementsの途中
-        // shared elements http://googledevjp.blogspot.jp/2014/11/android.html
-        // String transitionName = getString(R.string.transition_top_annei);
-        // ActivityOptionsCompat options =
-        // ActivityOptionsCompat.makeSceneTransitionAnimation(this,)
         startActivity(intent);
     }
 
