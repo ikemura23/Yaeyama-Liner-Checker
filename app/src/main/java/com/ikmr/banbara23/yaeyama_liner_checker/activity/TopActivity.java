7ae3668733b592d21d3a6d546edcf8fa107e8053
池村 和剛 <kazutakai@tt-com.net>
2016-01-17 01:16:02 +0900

    #43 トップ画面のリニューアル

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/activity/TopActivity.java
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/activity/TopActivity.java
@@ -11,12 +11,52 @@ import com.ikmr.banbara23.yaeyama_liner_checker.R;
 import com.ikmr.banbara23.yaeyama_liner_checker.entity.Company;
 import com.ikmr.banbara23.yaeyama_liner_checker.timetable.TimeTableActivity;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * トップActivity
+ */
 public class TopActivity extends BaseActivity {
+
+    @OnClick(R.id.top_activity_annei)
+    void anneiClick(View view) {
+        Intent intent = new Intent(this, StatusListActivity.class);
+        intent.putExtra(StatusListActivity.PARAM_COMPANY, Company.ANNEI);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.top_activity_ykf)
+    void ykfClick(View view) {
+        Intent intent = new Intent(this, StatusListActivity.class);
+        intent.putExtra(StatusListActivity.PARAM_COMPANY, Company.YKF);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.top_activity_dream)
+    void dreamClick(View view) {
+        Intent intent = new Intent(this, StatusListActivity.class);
+        intent.putExtra(StatusListActivity.PARAM_COMPANY, Company.DREAM);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.top_activity_timetable)
+    void timeTableClick(View view) {
+        Intent intent = new Intent(this, TimeTableActivity.class);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.top_activity_app)
+    void otherClick(View view) {
+        Intent intent = new Intent(this, OtherActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top);
+        ButterKnife.bind(this);
     }
 
     @Override
@@ -34,58 +74,5 @@ public class TopActivity extends BaseActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * 安栄クリック
-     * 
-     * @param view 安栄観光ボタン
-     */
-    public void anneiClick(View view) {
-        Intent intent = new Intent(this, StatusListActivity.class);
-        intent.putExtra(StatusListActivity.PARAM_COMPANY, Company.ANNEI);
-        startActivity(intent);
-    }
-
-    /**
-     * 八重山観光フェリークリック
-     * 
-     * @param view
-     */
-    public void ykfClick(View view) {
-        Intent intent = new Intent(this, StatusListActivity.class);
-        intent.putExtra(StatusListActivity.PARAM_COMPANY, Company.YKF);
-        startActivity(intent);
-    }
-
-    /**
-     * 八重山観光フェリークリック
-     *
-     * @param view
-     */
-    public void dreamClick(View view) {
-        Intent intent = new Intent(this, StatusListActivity.class);
-        intent.putExtra(StatusListActivity.PARAM_COMPANY, Company.DREAM);
-        startActivity(intent);
-    }
-
-    /**
-     * 時刻表クリック
-     *
-     * @param view
-     */
-    public void timeTableClick(View view) {
-        Intent intent = new Intent(this, TimeTableActivity.class);
-        startActivity(intent);
-    }
-
-    /**
-     * 八重山観光フェリークリック
-     *
-     * @param view
-     */
-    public void otherClick(View view) {
-        Intent intent = new Intent(this, OtherActivity.class);
-        startActivity(intent);
     }
 }
