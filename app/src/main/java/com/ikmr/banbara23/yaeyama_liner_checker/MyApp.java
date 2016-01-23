6fc8d483605d5c4971a5c611748822007d34b1b1
池村 和剛 <kazutakai@tt-com.net>
2016-01-23 11:58:48 +0900

    #2 fablicプラグイン導入

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/MyApp.java
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/MyApp.java
@@ -1,9 +1,13 @@
 
 package com.ikmr.banbara23.yaeyama_liner_checker;
 
+import timber.log.Timber;
+
 import android.app.Application;
 
-import timber.log.Timber;
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by banbara23 on 15/09/19.
@@ -12,6 +16,7 @@ public class MyApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         Timber.plant(new Timber.DebugTree());
     }
 }
