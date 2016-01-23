21a6508c609cb5c08376bb53faa56b81f85018bc
池村 和剛 <kazutakai@tt-com.net>
2016-01-23 17:41:43 +0900

    #2 デバッグビルドでclashlyticsが飛ばないよう修正

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/MyApp.java
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/MyApp.java
@@ -1,13 +1,13 @@
 
 package com.ikmr.banbara23.yaeyama_liner_checker;
 
-import timber.log.Timber;
-
 import android.app.Application;
 
 import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.core.CrashlyticsCore;
 
 import io.fabric.sdk.android.Fabric;
+import timber.log.Timber;
 
 /**
  * Created by banbara23 on 15/09/19.
@@ -16,7 +16,8 @@ public class MyApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Fabric.with(this, new Crashlytics());
+        CrashlyticsCore core = new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build();
+        Fabric.with(this, new Crashlytics.Builder().core(core).build());
         Timber.plant(new Timber.DebugTree());
     }
 }
