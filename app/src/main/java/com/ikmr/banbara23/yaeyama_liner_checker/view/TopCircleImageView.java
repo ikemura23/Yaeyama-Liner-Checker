cbcff25ca1ea589aa10903c4796baa9f963e72dd
池村 和剛 <kazutakai@tt-com.net>
2016-02-07 08:20:33 +0900

    #78 ダサいのでトップ画面丸いViewから画像を外す

---
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/view/TopCircleImageView.java
@@ -0,0 +1,19 @@
+
+package com.ikmr.banbara23.yaeyama_liner_checker.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by banbara23 on 16/02/07.
+ */
+public class TopCircleImageView extends ImageView {
+    public TopCircleImageView(Context context) {
+        super(context);
+    }
+
+    public TopCircleImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
