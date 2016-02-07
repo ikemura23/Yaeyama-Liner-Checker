733de38ce9382680f2f4d0fd2dd6f9a581e623cf
池村 和剛 <kazutakai@tt-com.net>
2016-02-07 09:01:32 +0900

    #89 アプリ情報を設定に移動

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/fragment/SettingFragment.java
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/fragment/SettingFragment.java
@@ -1,8 +1,11 @@
 
 package com.ikmr.banbara23.yaeyama_liner_checker.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
 
 import com.ikmr.banbara23.yaeyama_liner_checker.R;
 
@@ -15,5 +18,17 @@ public class SettingFragment extends PreferenceFragment {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.preferences);
+
+        PreferenceScreen preferenceScreen = (PreferenceScreen) findPreference("info_preference");
+        preferenceScreen.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Intent intent = new Intent(
+                        getActivity(),
+                        com.ikmr.banbara23.yaeyama_liner_checker.activity.OtherActivity.class);
+                startActivity(intent);
+                return true;
+            }
+        });
     }
 }
