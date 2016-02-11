9d2acae0cceacd2474ebd584d2c501b224b7032f
池村 和剛 <kazutakai@tt-com.net>
2016-02-11 12:07:20 +0900

    #46 コンフリクト対応

---
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/timetable/TimeTableFragment.java
@@ -0,0 +1,26 @@
+
+package com.ikmr.banbara23.yaeyama_liner_checker.timetable;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ikmr.banbara23.yaeyama_liner_checker.fragment.BaseFragment;
+
+/**
+ * 時刻表フラグメント
+ */
+public class TimeTableFragment extends BaseFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
