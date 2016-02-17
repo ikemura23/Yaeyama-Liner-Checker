4a8ce294e399515b1f3f203e00d4515b5f2b162f
池村 和剛 <kazutakai@tt-com.net>
2016-02-17 23:36:58 +0900

    #46 時刻表 安栄の表示切り替え実装

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/timetable/TimeTableFragment.java
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/timetable/TimeTableFragment.java
@@ -9,10 +9,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import com.ikmr.banbara23.yaeyama_liner_checker.R;
 import com.ikmr.banbara23.yaeyama_liner_checker.entity.Company;
+import com.ikmr.banbara23.yaeyama_liner_checker.entity.Port;
+import com.ikmr.banbara23.yaeyama_liner_checker.timetable.annei.AnneiTimeTableView;
+import com.ikmr.banbara23.yaeyama_liner_checker.timetable.dream.DreamTimeTableView;
+import com.ikmr.banbara23.yaeyama_liner_checker.timetable.ykf.YkfTimeTableView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -24,6 +29,14 @@ public class TimeTableFragment extends Fragment implements AdapterView.OnItemSel
 
     @Bind(R.id.activity_timetable_tab_spinner)
     Spinner mSpinner;
+    @Bind(R.id.fragment_timetable_annei_view)
+    AnneiTimeTableView mAnneiTimeTableView;
+
+    @Bind(R.id.fragment_timetable_ykf_view)
+    YkfTimeTableView mYkfTimeTableView;
+
+    @Bind(R.id.fragment_timetable_dream_view)
+    DreamTimeTableView mDreamTimeTableView;
 
     public static TimeTableFragment NewInstance(Company company) {
         TimeTableFragment fragment = new TimeTableFragment();
@@ -43,17 +56,140 @@ public class TimeTableFragment extends Fragment implements AdapterView.OnItemSel
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_timetable, container, false);
         ButterKnife.bind(this, view);
-        mSpinner.setOnItemSelectedListener(this);
+        setCommpanyToView();
+        createSpinner();
+
         return view;
+    }
+
+    private void setCommpanyToView() {
+        Company company = (Company) getArguments().getSerializable(TimeTableFragment.class.getCanonicalName());
+        if (company == null) {
+            return;
+        }
+        Log.d("TimeTableFragment", "company:" + company);
+        switch (company) {
+            case ANNEI:
+                mAnneiTimeTableView.setVisibility(View.VISIBLE);
+                break;
+            case YKF:
+                mYkfTimeTableView.setVisibility(View.VISIBLE);
+                break;
+            case DREAM:
+                mDreamTimeTableView.setVisibility(View.VISIBLE);
+                break;
+        }
+    }
+
+    private void createSpinner() {
+        mSpinner.setOnItemSelectedListener(this);
+        ArrayAdapter<String> mAdapter = new ArrayAdapter<>(getActivity().getApplicationContext(), android.R.layout.simple_spinner_item);
+        mAdapter.addAll(getPortList());
+        mAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    }
+
+    private String[] getPortList() {
+        Company company = (Company) getArguments().getSerializable(TimeTableFragment.class.getCanonicalName());
+        if (company == null) {
+            return new String[0];
+        }
+        switch (company) {
+            case ANNEI:
+                return getResources().getStringArray(R.array.annei_port_list);
+            case YKF:
+                return getResources().getStringArray(R.array.ykf_port_list);
+            case DREAM:
+                return getResources().getStringArray(R.array.annei_port_list);
+        }
+        return new String[0];
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        Log.d("TimeTableFragment", "position:" + position);
+        Company company = (Company) getArguments().getSerializable(TimeTableFragment.class.getCanonicalName());
+        if (company == null) {
+            return;
+        }
+        switch (company) {
+            case ANNEI:
+                ;
+                mAnneiTimeTableView.switchPortView(getAnneiPort(position));
+                break;
+            case YKF:
+                mYkfTimeTableView.switchPortView(getYkfPort(position));
+                break;
+            case DREAM:
+                mDreamTimeTableView.switchPortView(getDreamPort(position));
+                break;
+        }
+    }
+
+    private Port getAnneiPort(int position) {
+        switch (position) {
+            case 0:
+                return Port.TAKETOMI;
+            case 1:
+                return Port.KOHAMA;
+            case 2:
+                return Port.KUROSHIMA;
+            case 3:
+                return Port.OOHARA;
+            case 4:
+                return Port.UEHARA;
+            case 5:
+                return Port.HATOMA;
+            case 6:
+                return Port.HATERUMA;
+            default:
+                return Port.TAKETOMI;
+        }
+    }
+
+    private Port getYkfPort(int position) {
+        switch (position) {
+            case 0:
+                return Port.TAKETOMI;
+            case 1:
+                return Port.KOHAMA;
+            case 2:
+                return Port.KUROSHIMA;
+            case 3:
+                return Port.OOHARA;
+            case 4:
+                return Port.UEHARA;
+            case 5:
+                return Port.HATERUMA;
+            default:
+                return Port.TAKETOMI;
+
+        }
+    }
+
+    private Port getDreamPort(int position) {
+        switch (position) {
+            case 0:
+                return Port.TAKETOMI;
+            case 1:
+                return Port.KOHAMA;
+            case 2:
+                return Port.KUROSHIMA;
+            case 3:
+                return Port.OOHARA;
+            case 4:
+                return Port.HATOMA_UEHARA;
+            default:
+                return Port.TAKETOMI;
+        }
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
     }
 }
--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/timetable/annei/AnneiTimeTableView.java
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/timetable/annei/AnneiTimeTableView.java
@@ -10,6 +10,8 @@ import android.widget.FrameLayout;
 import com.ikmr.banbara23.yaeyama_liner_checker.R;
 import com.ikmr.banbara23.yaeyama_liner_checker.entity.Port;
 
+import java.util.List;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
@@ -20,18 +22,30 @@ public class AnneiTimeTableView extends FrameLayout {
 
     @Bind(R.id.view_time_table_annei_taketomi)
     AnneiTimeTableTaketomiView mAnneiTimeTableTaketomiView;
-    @Bind(R.id.view_time_table_annei_uehara)
-    AnneiTimeTableUeharaView mAnneiTimeTableUeharaView;
-    @Bind(R.id.view_time_table_annei_oohara)
-    AnneiTimeTableOoharaView mAnneiTimeTableOoharaView;
     @Bind(R.id.view_time_table_annei_kohama)
     AnneiTimeTableKohamaView mAnneiTimeTableKohamaView;
     @Bind(R.id.view_time_table_annei_kurhoshima)
     AnneiTimeTableKuroshimaView mAnneiTimeTableKuroshimaView;
+    @Bind(R.id.view_time_table_annei_oohara)
+    AnneiTimeTableOoharaView mAnneiTimeTableOoharaView;
+    @Bind(R.id.view_time_table_annei_uehara)
+    AnneiTimeTableUeharaView mAnneiTimeTableUeharaView;
     @Bind(R.id.view_time_table_annei_hatoma)
     AnneiTimeTableHatomaView mAnneiTimeTableHatomaView;
     @Bind(R.id.view_time_table_annei_hateruma)
     AnneiTimeTableHaterumaView mAnneiTimeTableHaterumaView;
+
+    @Bind({
+            R.id.view_time_table_annei_taketomi,
+            R.id.view_time_table_annei_kohama,
+            R.id.view_time_table_annei_kurhoshima,
+            R.id.view_time_table_annei_oohara,
+            R.id.view_time_table_annei_uehara,
+            R.id.view_time_table_annei_hatoma,
+            R.id.view_time_table_annei_hateruma
+
+    })
+    List<View> aneiViews;
 
     public AnneiTimeTableView(Context context) {
         super(context);
@@ -43,16 +57,26 @@ public class AnneiTimeTableView extends FrameLayout {
         ButterKnife.bind(this, layout);
     }
 
+    /***
+     * 非表示一括切り替え用
+     */
+    protected static final ButterKnife.Action<View> DISABLE = new ButterKnife.Action<View>() {
+        @Override
+        public void apply(View view, int index) {
+            view.setVisibility(GONE);
+        }
+    };
+
     /**
      * 会社で表示を切り替え
-     * 
+     *
      * @param port 港
      */
     public void switchPortView(Port port) {
         if (port == null) {
             return;
         }
-
+        ButterKnife.apply(aneiViews, DISABLE);
         switch (port) {
             case TAKETOMI:
                 mAnneiTimeTableTaketomiView.setVisibility(VISIBLE);
